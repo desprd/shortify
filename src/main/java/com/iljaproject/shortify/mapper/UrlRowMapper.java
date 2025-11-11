d67bf913f91e4361865f9e2b4e9be807d9a60ff0
Ilya-Kanstanchuk <ilyakonst95@gmail.com>
2025-11-11 16:29:48 +0100

    Refactor Url from class to record

--- src/main/java/com/iljaproject/shortify/mapper/UrlRowMapper.java
+++ src/main/java/com/iljaproject/shortify/mapper/UrlRowMapper.java
@@ -12,13 +12,13 @@ import java.time.LocalDateTime;
 public class UrlRowMapper implements RowMapper<Url> {
     @Override
     public Url mapRow(ResultSet rs, int rowNum) throws SQLException {
-        Url url = new Url();
-        url.setId(rs.getLong("id"));
-        url.setOriginalUrl(rs.getString("original_url"));
-        url.setShortCode(rs.getString("short_code"));
-        url.setCreatedAt(rs.getObject("created_at", LocalDateTime.class));
-        url.setLastAccessed(rs.getObject("last_accessed", LocalDateTime.class));
-        url.setClickCount(rs.getLong("click_count"));
-        return url;
+        return new Url(
+                rs.getLong("id"),
+                rs.getString("original_url"),
+                rs.getString("short_code"),
+                rs.getObject("created_at", LocalDateTime.class),
+                rs.getObject("last_accessed", LocalDateTime.class),
+                rs.getLong("click_count")
+        );
     }
 }
--- src/main/java/com/iljaproject/shortify/model/Url.java
+++ src/main/java/com/iljaproject/shortify/model/Url.java
@@ -3,90 +3,9 @@ package com.iljaproject.shortify.model;
 import java.time.LocalDateTime;
 import java.util.Objects;
 
-public class Url {
-
-    private Long id;
-    private String originalUrl;
-    private String shortCode;
-    private LocalDateTime createdAt;
-    private LocalDateTime lastAccessed;
-    private Long clickCount;
-
-    public Url(){};
-
-    public Url(
-            Long id,
-            String originalUrl,
-            String shortCode,
-            LocalDateTime createdAt,
-            LocalDateTime lastAccessed,
-            Long clickCount
-    ) {
-        this.id = id;
-        this.originalUrl = originalUrl;
-        this.shortCode = shortCode;
-        this.createdAt = createdAt;
-        this.lastAccessed = lastAccessed;
-        this.clickCount = clickCount;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getOriginalUrl() {
-        return originalUrl;
-    }
-
-    public void setOriginalUrl(String originalUrl) {
-        this.originalUrl = originalUrl;
-    }
-
-    public String getShortCode() {
-        return shortCode;
-    }
-
-    public void setShortCode(String shortCode) {
-        this.shortCode = shortCode;
-    }
-
-    public LocalDateTime getCreatedAt() {
-        return createdAt;
-    }
-
-    public void setCreatedAt(LocalDateTime createdAt) {
-        this.createdAt = createdAt;
-    }
-
-    public LocalDateTime getLastAccessed() {
-        return lastAccessed;
-    }
-
-    public void setLastAccessed(LocalDateTime lastAccessed) {
-        this.lastAccessed = lastAccessed;
-    }
-
-    public Long getClickCount() {
-        return clickCount;
-    }
-
-    public void setClickCount(Long clickCount) {
-        this.clickCount = clickCount;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (o == null || getClass() != o.getClass()) return false;
-        Url url = (Url) o;
-        return Objects.equals(id, url.id) && Objects.equals(originalUrl, url.originalUrl) && Objects.equals(shortCode, url.shortCode) && Objects.equals(createdAt, url.createdAt) && Objects.equals(lastAccessed, url.lastAccessed) && Objects.equals(clickCount, url.clickCount);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, originalUrl, shortCode, createdAt, lastAccessed, clickCount);
-    }
-}
+public record Url( Long id,
+        String originalUrl,
+        String shortCode,
+        LocalDateTime createdAt,
+        LocalDateTime lastAccessed,
+        Long clickCount) {}
